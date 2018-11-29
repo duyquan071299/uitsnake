db4ad8e62c120e5bab35dae8d66726b264e36c30
hoangsadn <nguyenthanhtrungltv@gmail.com>
2018-11-29 17:37:14 +0700

    add sound

---
+++ MusicEffect.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMPLib;
+
+namespace UIT_Snake
+{
+    class SoundEffect
+    {
+        WindowsMediaPlayer sound;
+
+        public SoundEffect(string _filePath)
+        {
+            sound = new WindowsMediaPlayer();
+            sound.URL = _filePath;
+            sound.controls.play();
+        }
+
+        public void Play()
+        {
+            sound.controls.play();
+        }
+
+        public void Stop()
+        {
+            sound.controls.stop();
+        }
+
+        public void Pause()
+        {
+            sound.controls.pause();
+        }
+
+        public void Resume()
+        {
+            if (sound.status == "Paused")
+                sound.controls.play();
+        }
+    }
+}
+
